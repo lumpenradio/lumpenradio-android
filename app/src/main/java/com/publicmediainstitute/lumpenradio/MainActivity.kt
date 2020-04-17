e714850ac755cd2d67992c02ce742f43c00e0d0a
Anthony Avina <anthony.avina@snapsheet.me>
2020-04-16 22:51:45 -0500

    Working

--- app/src/main/java/com/publicmediainstitute/lumpenradio/MainActivity.kt
+++ app/src/main/java/com/publicmediainstitute/lumpenradio/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.publicmediainstitute.lumpenradio
 
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val LUMPEN_RADIO_URL = "http://mensajito.mx:8000/lumpen"
+    var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         radioButton.setOnClickListener {
+            radioButton.isClickable = false
             val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.lumpen_radio_audio_logo_nor)
             mediaPlayer?.start() // no need to call prepare(); create() does that for you
+            mediaPlayer?.setOnCompletionListener {
+                startRadio()
+            }
         }
+    }
+
+    private fun startRadio() {
+        radioButton.isClickable = true
+        backgroundImage.setImageDrawable(ContextCompat.getDrawable(
+            this,
+            R.drawable.background_on))
+
+        mediaPlayer = MediaPlayer()
+
+        // TODO: May want to version check and use AudioAttributes
+        /*
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        mediaPlayer?.setAudioAttributes(AudioAttributes(
+            ,
+
+        ))
+         */
+        mediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        mediaPlayer?.setDataSource(LUMPEN_RADIO_URL)
+        mediaPlayer?.setOnPreparedListener {
+            it.start()
+        }
+        mediaPlayer?.prepareAsync()
     }
 }
