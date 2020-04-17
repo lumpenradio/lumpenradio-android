146fc6a9f898695e6ada4cd7a186eb672964ee17
Anthony Avina <anthony.avina@snapsheet.me>
2020-04-16 22:06:41 -0500

    Made button

--- app/src/main/java/com/publicmediainstitute/lumpenradio/MainActivity.kt
+++ app/src/main/java/com/publicmediainstitute/lumpenradio/MainActivity.kt
@@ -2,11 +2,17 @@ package com.publicmediainstitute.lumpenradio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radioButton.setOnClickListener {
+            Toast.makeText(this, "Pressed button!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
