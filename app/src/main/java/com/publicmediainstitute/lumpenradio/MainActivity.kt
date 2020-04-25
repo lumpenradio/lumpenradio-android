55f8f423890726e855b56740b6a6cd4364a48378
Anthony Avina <anthony@aavina.com>
2020-04-25 16:09:55 -0500

    Added updates
    Notification showing up but starting a new MainActivity instead of returning to existing

--- app/src/main/java/com/publicmediainstitute/lumpenradio/MainActivity.kt
+++ app/src/main/java/com/publicmediainstitute/lumpenradio/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.publicmediainstitute.lumpenradio
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     var mediaPlayer: MediaPlayer? = null
     var introMediaPlayer: MediaPlayer? = null
     var radioReady = false
+    val notificationId = 2020
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +41,28 @@ class MainActivity : AppCompatActivity() {
         radioButton.setOnClickListener {
             radioButtonClicked()
         }
+
+        // Create notification channel and notification
+        createNotificationChannel()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Start the notification
+        mediaPlayer?.let {
+            if (it.isPlaying) {
+                with(NotificationManagerCompat.from(this)) {
+                    notify(notificationId, constructNotification())
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // TODO: Clear the notification
     }
 
     private fun radioButtonClicked() {
@@ -132,6 +163,52 @@ class MainActivity : AppCompatActivity() {
                     R.drawable.background_on))
                 it.start()
             }
+        }
+    }
+
+    private fun getChannelId(): String {
+        return getString(R.string.notification_channel_id)
+    }
+
+    /**
+     * Creates a notification for user to interact with for controlling the radio
+     */
+    private fun constructNotification(): Notification {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,
+            0,
+            intent,
+            0)
+
+        val builder = NotificationCompat.Builder(this, getChannelId())
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.notification_content_text))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        return builder.build()
+    }
+
+    /**
+     * Create a notification channel. Only used in Android 8.0+
+     * Reference: https://developer.android.com/training/notify-user/build-notification
+     */
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_name)
+            val descriptionText = getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(getChannelId(), name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManger: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManger.createNotificationChannel(channel)
         }
     }
 }
